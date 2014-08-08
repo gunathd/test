d39365af8b3b370aa0cf3e6f70e83d4adebb9b12
gunathd <dinusha.gunathilaka@autodesk.com>
2014-08-08 10:06:29 +0800

    automation scripts

--- automation/forgotpassword.py
+++ automation/forgotpassword.py
@@ -11,24 +11,30 @@ driver.maximize_window()
 driver.implicitly_wait(10)
 
 #Open app
-driver.get('http://developer-stg.autodesk.com')
+driver.get('http://developer-local.autodesk.com:8000/')
 
-#------------------Manage login(start)-----------------------
+#------------------Manage forgot password(start)-----------------------
 login = driver.find_element_by_id("btn_oxygen_login")
 login.click()
 
-
-frameID = driver.find_element_by_css_selector("div[class=fancybox-inner]>iframe")
+frameID = driver.find_element_by_id("oxygen_ifr")
 driver.switch_to_frame(frameID)
 
 forgotpassword= driver.find_element_by_link_text('Forgot your password?')
 forgotpassword.click()
 
-#frameID = driver.find_element_by_css_selector("iframe")
-#driver.switch_to_frame(frameID)
-frameID = driver.find_element_by_css_selector("body[class= iframe_mode ForgotCredentials ]")
-driver.switch_to_frame(frameID)
+driver.switch_to_default_content()
 
+frame = driver.find_element_by_css_selector("#oxygen-container>iframe")
+driver.switch_to_frame(frame)
 
 username = driver.find_element_by_id("username_str")
-username.send_keys("selenium505@gmail.com")
+username.send_keys("seleniumtest222@gmail.com")
+
+to_continue1 = driver.find_element_by_css_selector("button[class='ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only']")
+to_continue1.click()
+
+to_continue2 = driver.find_element_by_css_selector("a[class='ui-button default ui-widget ui-state-default ui-corner-all ui-button-text-only']")
+to_continue2.click()
+#------------------Manage forgot password(finish)-----------------------
+
